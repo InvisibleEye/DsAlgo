359a7ca22edb147c5871da414a24903d2e6e92f6
InvisibleEye <sr.tanivr8943@gmail.com>
2023-06-17 12:26:10 -0400

    SYSC2100 Assignment 1 recursion

---
+++ Assignment_1/src/DiskUsage.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class DiskUsage {
+	public static long diskUsage (File root)
+	{
+		long size = root.length();
+		return size;
+	}
+	public static void main (String args[]) {
+		File f = new 
+	}
+
+}
